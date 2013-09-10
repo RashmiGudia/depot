a98ff578e1613bba203a168f6a387783d1dd7212
RashmiGudia <7101990rashmi@gmail.com>
2013-09-10 19:50:50 +0530

    Validation!

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+    test "product price must be positive" do
+  product = Product.new(:title => "ma book",
+                        :description => "op",
+                        :image_url => "po.jpg")
+  product.price = -1
+  assert product.invalid?
+  assert_equal "must be greater than or equal to 0.01",
+  product.errors[:price].join('; ')
+
+ product.price = 0
+ assert product.invalid?
+ assert_equal "must be greater than or equal to 0.01"
+  product.errors[:price].join('; ')
+
+ product.price = 1
+ assert product.valid?
+
+ end 
 end
